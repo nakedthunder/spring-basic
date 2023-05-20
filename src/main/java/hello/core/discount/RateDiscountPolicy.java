410d0f2d61e6dc2ee721985b8b80b2b5dea5a50d
Jess <nakedthunder00@gmail.com>
2023-05-20 11:15:46 +0900

    add RateDiscountPolicy to AppConfig

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,24 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+    /*
+    * 1. 정책
+    *  - VIP이면 10퍼센트 할인을 해야한다.
+    * 2. 10퍼센트 가격 구하기
+    * - price * dsiocountPrecent / 100;
+    * */
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
