370c5dd2d3d06692b1b0aacfca1c92e542088ad0
Jess <nakedthunder00@gmail.com>
2023-05-21 19:25:20 +0900

    changed java to Spring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,24 +8,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     // 중복도 제거가 되고, 역할과 구현클래스가 한눈에 들어온다
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
-
         return new RateDiscountPolicy();
         //return new FixDiscountPolicy();
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,17 +4,29 @@ import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        // 기존에 MemberServiceIml를 직접 생성했다면 ~
+       /* 자바코드로 appConfig에 접근했다면 아래 스프링으로 변환
         AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
-        //MemberService memberService = new MemberServiceImpl();
+        */
+
+        /*
+        * ApplicationContext
+        *
+        * AnnotationConfigApplicationContext
+        *
+        * getBean
+        * */
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
         // command option v (window control+alt+v)
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
-        // 순수한 자바코드로만 개발...좋은 Junit 테스트 프레임워크를 쓰자
+        // 순수한 자바코드로 테스트 -> 좋은 Junit 테스트 프레임워크를 쓰자
         Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
         System.out.println("findMember = " + findMember.getName());
