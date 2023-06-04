5f022c0f7a4c03552a81ff15d0f12aa0379b840d
Jess <nakedthunder00@gmail.com>
2023-06-05 05:48:31 +0900

    Autowired

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
         // 수동으로 등록한 AppConfig 는 빼준다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
