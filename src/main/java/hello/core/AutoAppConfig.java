2b12954d96d5fc1817d21d1179e05b31eedf6804
Jess <nakedthunder00@gmail.com>
2023-07-21 05:36:39 +0900

    Easy to use polymorphism by spring bean

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
+        //basePackages = "hello.core.member", 지정한 패키지 위치만 빈등록
         // 수동으로 등록한 AppConfig 는 빼준다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -2,7 +2,9 @@ package hello.core.autowired;
 
 import hello.core.AutoAppConfig;
 import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -18,8 +20,16 @@ public class AllBeanTest {
         // 스프링 컨테이너를 등럭하면서
         ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
 
-        DiscountService bean = ac.getBean(DiscountService.class);
+        // 0721 비지니스 로직
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
 
+        Assertions.assertThat(discountService).isInstanceOf(DiscountService.class);
+        Assertions.assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        Assertions.assertThat(rateDiscountPrice).isEqualTo(2000);
     }
 
     // 새로운 서비스 생성
@@ -35,6 +45,11 @@ public class AllBeanTest {
             this.policies = policies;
             System.out.println("policyMap= " + policyMap);
             System.out.println("policies= "+ policies);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
         }
 
         // 생성자 주입
